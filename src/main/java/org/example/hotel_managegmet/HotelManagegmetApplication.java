ea0d9331a56439d23b5112b8872f78d8791d6cba
Egodz93 <than624264@gmail.com>
2026-01-26 09:23:16 +0700

    initial project setup

---
+++ src/main/java/org/example/hotel_managegmet/HotelManagegmetApplication.java
@@ -0,0 +1,13 @@
+package org.example.hotel_managegmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagegmetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagegmetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hotel_managegmet/HotelManagegmetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hotel_managegmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagegmetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
